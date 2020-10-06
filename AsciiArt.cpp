4b9e6ac2f3b974b3d0724aa271c470998979b6a7
Mehdi Aarab <59884423+Mehdi485@users.noreply.github.com>
2020-10-06 14:59:15 +0200

    Create AsciiArt.cpp

---
+++ AsciiArt.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <map>
+
+using namespace std;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+
+int main()
+{
+    int L;
+    map<char, vector<string>> characters;
+    cin >> L; cin.ignore();
+    int H;
+    cin >> H; cin.ignore();
+    string T;
+    getline(cin, T);
+    for (int i = 0; i < H; i++) {
+        string ROW;
+        getline(cin, ROW);
+        characters['?'].push_back(ROW.substr(26*L, L));
+        for (int j = 0; j < 26; j++)
+            characters['A' + j].push_back(ROW.substr(j*L, L));
+    }
+        for (int i = 0; i < H; i++)
+    {
+        for (auto c : T)
+        {
+            if ((c >= 'a') && (c <= 'z'))
+                c += ('A' - 'a');
+            else if ((c < 'A') || (c > 'Z'))
+                c = '?';
+            
+            cout << characters[c][i];
+        }
+        
+        cout << endl;
+    }
+
+    // Write an action using cout. DON'T FORGET THE "<< endl"
+    // To debug: cerr << "Debug messages..." << endl;
+
+   
+}
