8b88627d001b466368c458217f6548f73789b81d
Mehdi Aarab <59884423+Mehdi485@users.noreply.github.com>
2020-10-06 16:35:44 +0200

    Update LaDescente.cpp

--- LaDescente.cpp
+++ LaDescente.cpp
@@ -6,13 +6,6 @@
 
 using namespace std;
 
-/**
- * The while loop represents the game.
- * Each iteration represents a turn of the game
- * where you are given inputs (the heights of the mountains)
- * and where you have to print an output (the index of the mountain to fire on)
- * The inputs you are given are automatically updated according to your last actions.
- **/
 
 int main()
 {
